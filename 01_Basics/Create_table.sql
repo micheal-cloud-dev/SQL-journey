0e53cc3b551ee4ccc49cd8771dff300327525627
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-07 20:14:31 +0530

    Modified
    Removed unnecessary alterations and inserts from Create_table.sql.

--- 01_Basics/Create_table.sql
+++ 01_Basics/Create_table.sql
@@ -9,14 +9,3 @@ experiance varchar(20)
 );
 select * from employee;
 
-alter table employee
-add contact varchar(10);
-
-insert into employee
-values ( 1 , "Micheal" , "fullstack"  , " 2_years" , "9082697041");
-
-insert into employee
-values
-     ( 2 , "Edison" , "cloud dev" , "1_year" , "9788913811"),
-     ( 3 , "watson" , "frontend" , "3_year" , "8883080013" );
-
