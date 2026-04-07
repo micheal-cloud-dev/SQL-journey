42133faeacfbcca924e5ba54ba3e44de8dac0754
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-07 19:45:43 +0530

    Create company database and employee table
    Created a database and table for employee records, added a contact column, and inserted initial employee data.

--- 01_Basics/Create_table.sql
+++ 01_Basics/Create_table.sql
@@ -1,1 +1,22 @@
+create database company;
+use company;
+
+create table employee(
+id int,
+emp_name varchar(15),
+skills varchar(25),
+experiance varchar(20)
+);
+select * from employee;
+
+alter table employee
+add contact varchar(10);
+
+insert into employee
+values ( 1 , "Micheal" , "fullstack"  , " 2_years" , "9082697041");
+
+insert into employee
+values
+     ( 2 , "Edison" , "cloud dev" , "1_year" , "9788913811"),
+     ( 3 , "watson" , "frontend" , "3_year" , "8883080013" );
 
