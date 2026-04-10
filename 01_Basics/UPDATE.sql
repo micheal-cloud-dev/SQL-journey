f9b37a79f20441a5af2996022a76296facee0f87
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-10 19:52:08 +0530

    Initialize college database and students table
    Created a database and table for students, added a contact column, and inserted initial data.

--- 01_Basics/UPDATE.sql
+++ 01_Basics/UPDATE.sql
@@ -1,1 +1,26 @@
+create database college;
+use college;
+
+create table students(
+   reg_no int,
+   s_name varchar(20),
+   department varchar(20)
+);
+select * from students;
+
+alter table students
+add contact varchar(10);
+
+insert into students
+values( 1 , "Micheal" , "CSE(AIML)" ),
+	  ( 2 , "Edison" , "EEE" );
+update students
+set contact = "9082697041"
+where reg_no = 1;
+
+set sql_safe_updates=0;
+
+update students
+set contact = "9035173086"
+where reg_no = 2;
 
