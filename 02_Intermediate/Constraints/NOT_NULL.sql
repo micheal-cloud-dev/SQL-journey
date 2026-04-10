15ee92b33c33df5283e418caaa5daf5b2a3c61ff
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-10 21:16:55 +0530

    Create stores database and product table with constraints
    Create a new database and table with NOT NULL constraint for p_name. Insert sample data and modify p_name to enforce NOT NULL.

--- 02_Intermediate/Constraints/NOT_NULL.sql
+++ 02_Intermediate/Constraints/NOT_NULL.sql
@@ -1,1 +1,21 @@
 
+create database stores;
+use stores;
+
+create table product(
+  p_id int,
+  p_name varchar(20 not null,
+  prize decimal(4,2)
+);
+select * from product;
+
+insert into product(p_id,prize)
+values (1 , 45.50);
+
+insert into product
+values( 2," ", 50.40);
+
+drop table product;
+
+alter table product
+modify p_name varchar(20) not null;
