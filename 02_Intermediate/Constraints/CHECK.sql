6b8a01879caf6fc9532aeef5ef4f3b66446639b3
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-10 21:42:40 +0530

    Set up product table with check constraint on prize
    Created a database and table for products with a check constraint on the prize. Inserted sample data and modified the check constraint.

--- 02_Intermediate/Constraints/CHECK.sql
+++ 02_Intermediate/Constraints/CHECK.sql
@@ -1,1 +1,25 @@
 
+create database stores;
+use stores;
+
+create table product(
+  p_id int,
+  p_name varchar(20),
+  prize decimal(4,2),
+  constraint check_prize check(prize < 100)
+);
+select * from product;
+
+insert into product
+values (1 , "rice", 45.50);
+
+insert into product
+values( 2,"wheat", 250.00);
+
+alter table product
+drop constraint check_prize;
+
+drop table product;
+
+alter table product
+add constraint check_prize check(prize < 100);
