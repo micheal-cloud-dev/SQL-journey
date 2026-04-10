59e4cd11cd9126706c9740a9d121e52e2fb2206e
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-10 21:01:34 +0530

    Create database and product table with unique constraint

--- 02_Intermediate/Constraints/UNIQUE.sql
+++ 02_Intermediate/Constraints/UNIQUE.sql
@@ -1,1 +1,21 @@
 
+create database stores;
+use stores;
+
+create table product(
+  p_id int,
+  p_name varchar(20)unique,
+  prize decimal(4,2)
+);
+select * from product;
+
+insert into product
+values (1 , "rice" , 45.50);
+
+insert into product
+values( 2 , "rice" , 50.40);
+
+drop table product;
+
+alter table product
+add constraint unique(p_name);
