1b1815a97b3027fead03b039e24e0947f3f5fc83
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-11 20:33:55 +0530

    Create management database and transactions table
    Create a database and a transactions table with primary key. Insert sample data and drop the table afterwards.

--- 02_Intermediate/Constraints/PRIMARY_KEY.sql
+++ 02_Intermediate/Constraints/PRIMARY_KEY.sql
@@ -1,1 +1,26 @@
+create database management;
+use management;
+
+create table transactions(
+  t_id int primary key auto_increment,
+  amount decimal(6,2)
+);
+select * from transactions;
+
+insert into transactions(amount)
+values( 1, 40.5 ),
+	  ( 1, 60.50);
+
+insert into transactions(amount)
+values( 100.20 ),
+      ( 200.20 );
+
+drop table transactions;
+
+alter table transactions
+add constraint primary key(t_id);
+
+alter table transactions
+modify t_id int auto_increment;
+
 
