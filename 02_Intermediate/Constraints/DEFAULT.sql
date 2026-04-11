f09b31b249a19d05f02023dff63427e5ff804d5a
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-11 19:55:15 +0530

    Initialize 'stores' database and 'product' table
    Created a new database 'stores' and a table 'product' with default price.

--- 02_Intermediate/Constraints/DEFAULT.sql
+++ 02_Intermediate/Constraints/DEFAULT.sql
@@ -1,1 +1,21 @@
+create database stores;
+use stores;
+
+create table product(
+ P_id int,
+ p_name varchar(20),
+ P_prize decimal(4,2) default 30
+);
+insert into product(p_id , p_name)
+values( 1 , "coconut oil" );
+
+select * from product;
+
+drop table product;
+
+alter table product
+alter p_prize set default 30;
+
+insert into product(p_id , p_name)
+values( 1 , "coconut oil" );
 
