5705036c45039ed427494cc54ee71f5d4537e88e
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-13 21:06:34 +0530

    Initialize company database and employee aggregate queries
    Created a database and table for employees, and added aggregate functions to query employee data.

--- 02_Intermediate/Functions/Aggregate_Function.sql
+++ 02_Intermediate/Functions/Aggregate_Function.sql
@@ -1,1 +1,36 @@
+create database company;
+use company;
+create table employees
+(
+    e_id int primary key auto_increment,
+    e_name varchar(20)not null,
+    e_age int not null,
+    e_salary int
+);
+insert into employees(e_name,e_age,e_salary)
+values( "Micheal", 18 , 30000),
+	  ( "Edison" , 20 , 50000),
+      ( "joson" , 22 , 80000);
+      
+select * from employees;
+describe employees;
+
+-- count function
+select count(e_name) as name from employees;
+
+-- max function
+select max(e_salary) as salary from employees
+where e_salary > 40000;
+
+-- min function
+select min(e_age) as Age from employees;
+
+-- sum function
+select sum(e_salary) as total from employees
+where e_salary >= 30000;
+
+-- avg function
+select avg(e_salary) as average_Age from employees
+where e_salary >= 30000;
+
 
