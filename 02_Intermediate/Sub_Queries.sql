2c1a31903f4db71367bab870b7403ac6b33bf7aa
micheal-cloud-dev <michealedison677@gmail.com>
2026-04-16 19:49:48 +0530

    Set up company database and employee table with queries
    Created a database and table for employees, inserted sample data, and added subqueries to retrieve specific employee information based on salary and age criteria.

--- 02_Intermediate/Sub_Queries.sql
+++ 02_Intermediate/Sub_Queries.sql
@@ -1,1 +1,30 @@
+create database company;
+use company;
+create table employees
+(
+  id int primary key auto_increment,
+  e_name varchar(20),
+  age int,
+  salary int
+);
+insert into employees(e_name, age , salary)
+values( "abishek", 22 , 20000 ),
+      ( "gokul" , 24 , 25000),
+      ( "watson" ,26 , 30000),
+      ( " judson", 28, 35000);
+      
+-- sub queries
+
+select e_name,salary from employees
+where salary > (select avg(salary)from employees);
+
+select * from employees;
+
+select * from employees
+where age < (select avg(age) from employees);
+
+select id, e_name from employees 
+where id = 
+(select id from employees
+where e_name = "gokul");
 
